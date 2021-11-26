fcac152d6db695ad0ff7747b416ce708596da572
Ana VR <varaty7@gmail.com>
2021-11-26 16:05:15 -0600

    Racing game

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let canvas, backgroundImage;
+
+let gameState = 0;
+let playerCount;
+let allPlayers;
+let distance = 0;
+let database;
+
+let form, player, game;
+
+let cars, car1, car2, car3, car4;
+
+let track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
